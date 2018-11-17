dc0c5710a06e75c2acd2d6dd36a07e542125ccb7
Samuel Braga <samuel.fantini.braga@hotmail.com>
2018-11-17 00:07:11 +0000

    Criando pequeno menu para vacilitar o periodo de desenvolviemento

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,21 @@
 using namespace std;
 
 #include "user/Session.hpp"
+#include "invetory/Product.hpp"
 
 using namespace std;
 using user::Session;
+using inventory::Product;
+
+int menu(){
+    int x;
+    cout << "Gerenciamento de Loja!" << endl;
+    cout << "1- Cadastro de Produtos" << endl;
+    cout << "0- Sair" << endl;
+    cout << "Digite sua opção: "; 
+    cin >> x;
+    return x;
+}
 
 void startLogin(Session** userSession) {
   *userSession = new Session("Testando", "");
@@ -16,6 +28,23 @@ int main() {
   Session *userSession = nullptr;
   startLogin(&userSession);
   cout << userSession->_username;
-
+  
+  int option = -1;
+  while (option != 0){
+    option = menu();
+    switch(option){
+      case 1:
+        string name;
+        string description;
+        cout << "Digite o nome do Produto: ";
+        cin >> name;
+        cout << "Digite a descricao do Produto";
+        cin >> description;
+        Product *newProduct = new Product (name, description);
+      default:
+        cout << "Opcao Invalida tente novamente!";
+        break;
+    }
+  }
 	return 0;
 }
