d47230041cc02a1dbd4c5f029e119039d4d97406
lucasrleandro <42347669+lucasrleandro@users.noreply.github.com>
2018-11-18 20:31:24 -0200

    Alterações | comentários na Stock

---
+++ Stock.hpp
@@ -0,0 +1,27 @@
+#ifndef STOCK_H
+#define STOCK_H
+
+#include <iostream>
+#include <string>
+#include <map>
+
+#include "Product.hpp"
+
+using namespace std;
+
+namespace inventory {
+	class Stock {
+	private:
+		map<int, Product*> _products;
+		void saveProducts();
+	public:
+		Stock();
+		void listProducts();
+		void removeProduct(int);
+		void addProduct(Product*);
+		void searchProductById(int);
+		void searchProductByName(string);
+	};
+}
+
+#endif
