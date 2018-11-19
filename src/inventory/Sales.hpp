7ed009d83c1504541a4ee04b07c24c4cfecf580f
lucasrleandro <42347669+lucasrleandro@users.noreply.github.com>
2018-11-19 17:36:29 -0200

    Classe SALES para gerenciar as vendas

---
+++ src/inventory/Sales.hpp
@@ -0,0 +1,29 @@
+#ifndef SALES_H
+#define SALES_H
+
+#include <iostream>
+#include <string>
+#include <map>
+
+#include "Sale.hpp"
+
+using namespace std;
+
+namespace inventory {
+	class Sales {
+	private:
+		map<int, Sale*> _sales;
+		//void saveSales();
+	public:
+		Sales();
+		void listSales();
+		void addSale(Sale*);
+        void cancelSale(int); // passar o id 
+		void searchSaleById(int);
+        void searchSalesByDate(string date);
+        void searchSalesByEmployee(int); // passar o id
+        void searchSalesByPrice(double);
+    };
+}
+
+#endif
