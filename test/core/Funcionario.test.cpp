cc4a2cf144ede4ef0de489409ca50c254504cab9
Samuel Braga <samuel.fantini.braga@hotmail.com>
2018-11-23 00:13:28 +0000

    Finalizando teste para Team

--- test/core/Funcionario.test.cpp
+++ test/core/Funcionario.test.cpp
@@ -4,6 +4,8 @@
 #include <string>
 
 #include "user/Manager.hpp"
+#include "user/Employee.hpp"
+#include "user/Team.hpp"
 
 using namespace user;
 
@@ -11,12 +13,63 @@ TEST_CASE("Teste criar Manager") {
 
   Person* userTest;
   
-  std::string name = "samuel";
-  std::string username = "samuelbraga";
+  std::string name = "manager";
+  std::string username = "manager";
   std::string password = "123";
 
   userTest = new Manager(name, username, password);
-  CHECK(userTest->getName() == "samuel");
-  CHECK(userTest->getUsername() == "samuelbraga");
+  CHECK(userTest->getName() == "manager");
+  CHECK(userTest->getUsername() == "manager");
   CHECK(userTest->checkPassword("123") == true);
+}
+
+TEST_CASE("Teste criar Employee") {
+
+  Person* userTest;
+  
+  std::string name = "employee";
+  std::string username = "employee";
+  std::string password = "123";
+
+  userTest = new Employee(name, username, password);
+  CHECK(userTest->getName() == "employee");
+  CHECK(userTest->getUsername() == "employee");
+  CHECK(userTest->checkPassword("123") == true);
+}
+
+TEST_CASE("Teste Listar Funcionarios") {
+
+  Team* team;
+  
+  team = new Team;
+
+  team->listUsers();
+}
+
+TEST_CASE("Adicionar funcionario Funcionarios") {
+
+  string name = "testeAddFunc";
+  string username = "teste";
+  string password = "123";
+  bool isManager = true;
+
+  Team* team;
+  team = new Team;
+  team->addPerson(name, username, password, isManager);
+  team->listUsers();
+}
+
+TEST_CASE("Deletetar funcionario Funcionarios") {
+
+  string name = "testeAddFunc";
+  string username = "teste";
+  string password = "123";
+  bool isManager = true;
+
+  Team* team;
+  team = new Team;
+  team->addPerson(name, username, password, isManager);
+  team->listUsers();
+  team->deletePerson(username);
+  team->listUsers();
 }
